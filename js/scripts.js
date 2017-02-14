4347dfd7a4fa8f347e092d4482b1e9d6878c2bea
Alex Lee and Colin Bloom <colin@>
2017-02-14 11:30:00 -0800

    add sections, styles, additional functions

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(function() {
+  $(".clickable").click(function() {
+    $(".walrus-showing").fadeToggle();
+    $(".walrus-hidden").fadeToggle();
+    $(".generals").slideToggle();
+    $(".awesome").slideToggle();
+  });
+  $("#boo").click(function(){
+    $("#peek").toggleClass("rest");
+  })
+});
